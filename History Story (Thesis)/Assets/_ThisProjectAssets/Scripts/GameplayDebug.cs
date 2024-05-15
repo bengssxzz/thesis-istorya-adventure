3bda165aa035bbeb9dcc14255cfb90e514b013f6
bengssxzz <bautistajr9999@yahoo.com>
2024-05-16 02:15:41 +0800

    Fixing slow timescale bug

--- History Story (Thesis)/Assets/_ThisProjectAssets/Scripts/GameplayDebug.cs
+++ History Story (Thesis)/Assets/_ThisProjectAssets/Scripts/GameplayDebug.cs
@@ -6,37 +6,48 @@ using Cysharp.Threading.Tasks;
 
 public class GameplayDebug : MonoBehaviour
 {
+    [SerializeField] private bool showDebug = true;
+
+
     [SerializeField] private TextMeshProUGUI timeScaleText;
     [SerializeField] private TextMeshProUGUI fpsDisplayText;
 
-    private float fpsRate;
-    private float updateTimer = 0.2f;
+    private float timer, refresh, avgFrameRate;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        timeScaleText.text = $"TimeScale: {Time.timeScale}";
-        fpsDisplayText.text = $"FPS: {UpdateFPSDisplay()}";
+        ShowDebugUI(showDebug);
+
+        if (showDebug)
+        {
+            timeScaleText.text = $"TimeScale: {Time.timeScale}";
+            fpsDisplayText.text = $"FPS: {UpdateFPSDisplay()}";
+        }
+
+    }
+
+    private void ShowDebugUI(bool show)
+    {
+        timeScaleText.gameObject.SetActive(show);
+        fpsDisplayText.gameObject.SetActive(show);
     }
 
     private float UpdateFPSDisplay()
     {
-        float fps = 0f;
-        updateTimer -= Time.deltaTime;
-        if (updateTimer <= 0f)
-        {
-            fpsRate = 1f / Time.unscaledDeltaTime;
-            fps = Mathf.Round(fpsRate);
-            updateTimer = 0.2f;
-        }
+        float timeLapse = Time.unscaledDeltaTime;
+        timer = timer <= 0 ? refresh : timer -= timeLapse;
 
-        return fps;
+        if (timer <= 0)
+            avgFrameRate = (int)(1f / timeLapse);
+
+        return avgFrameRate;
     }
 
 
--- History Story (Thesis)/Assets/_ThisProjectAssets/Scripts/Menu/Pause Menu/PauseMenu.cs
+++ History Story (Thesis)/Assets/_ThisProjectAssets/Scripts/Menu/Pause Menu/PauseMenu.cs
@@ -19,7 +19,16 @@ public class PauseMenu : MonoBehaviour
         mainMenuBtn.ButtonReleased.AddListener(MainMenu_Button);
     }
 
-   
+    private void OnEnable()
+    {
+        Time.timeScale = 0;
+    }
+    private void OnDisable()
+    {
+        Time.timeScale = 1;
+    }
+
+
 
     private void ResumeButton()
     {
